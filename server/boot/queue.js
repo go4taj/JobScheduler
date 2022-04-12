2ebd743bf424427f532e5b4560d4fc9e266a657d
Taj Ahmed <taj.ahmed@otssolutions.com>
2022-04-12 20:25:13 +0530

    Add retries,updatedAt field, add max retries for a job

--- server/boot/queue.js
+++ server/boot/queue.js
@@ -1,6 +1,8 @@
+/* eslint-disable max-len */
 'use strict';
 const later = require('@breejs/later');
 const constants = require('../constants.json');
+const retryCount = parseInt(process.env.RETRY_COUNT) || 1;
 
 module.exports = function queseProcessor(server) {
   var schedPeriod = later.parse.text('every 1 minutes');
@@ -8,25 +10,48 @@ module.exports = function queseProcessor(server) {
 
   function onTick() {
     server.models.jobs.findOne({
-      where: {status: constants.STATUS.IN_QUEUE},
+      where: {
+        status: {
+          inq: [constants.STATUS.FAILED, constants.STATUS.IN_PROCESS],
+        },
+        retries: {lt: retryCount},
+      },
       order: 'id asc',
-    }, (err, job)=>{
-      if (job) {
-        job.status = constants.STATUS.IN_PROCESS;
-        job.save((err, saved)=>{
-          setTimeout(()=>{
-            try {
-              if (saved.info.throwError) {
-                throw new Error('Invalid processing');
-              }
-              saved.status = constants.STATUS.COMPLETE;
-              saved.save();
-            } catch (error) {
-              saved.error = error.message;
-              saved.status = constants.STATUS.FAILED;
-              saved.save();
+    }, (err, jobInProgress)=>{
+      if (!jobInProgress || jobInProgress.status === constants.STATUS.FAILED) {
+        server.models.jobs.findOne({
+          where: {
+            status: {
+              inq: [constants.STATUS.FAILED, constants.STATUS.IN_QUEUE],
+            },
+            retries: {lt: retryCount},
+          },
+          order: 'id asc',
+        }, (err, job)=>{
+          if (job) {
+            if (job.status === constants.STATUS.FAILED) {
+              job.retries++;
             }
-          }, 5000);
+            job.status = constants.STATUS.IN_PROCESS;
+            job.updatedAt = new Date();
+            job.save((err, saved)=>{
+              setTimeout(()=>{
+                try {
+                  if (saved.info.throwError) {
+                    throw new Error('Invalid processing');
+                  }
+                  saved.status = constants.STATUS.COMPLETE;
+                  saved.updatedAt = new Date();
+                  saved.save();
+                } catch (error) {
+                  saved.updatedAt = new Date();
+                  saved.error = error.message;
+                  saved.status = constants.STATUS.FAILED;
+                  saved.save();
+                }
+              }, 5000);
+            });
+          }
         });
       }
     });
